589d3883cb99deffb31b49346de776f473df95e6
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2015-11-06 14:05:30 +0000

    add some processing steps

--- llvm/hellopimpl/HelloWorldPassImpl.cpp
+++ llvm/hellopimpl/HelloWorldPassImpl.cpp
@@ -1,17 +1,28 @@
 
 #include "HelloWorldPassImpl.hpp"
-//#include "HelloWorldPass.hpp"
+#include "HelloWorldPass.hpp"
 
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
-//#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/raw_ostream.h>
 
 namespace compor {
 
 HelloWorldPassImpl::HelloWorldPassImpl(HelloWorldPass *pass) : m_pass(pass) {}
 
 bool HelloWorldPassImpl::runOnFunction(llvm::Function &f) {
-  return true;
+  llvm::errs() << "pass : " << m_pass->getPassName() << "\n";
+  llvm::errs() << "function name : ";
+  llvm::errs().write_escaped(f.getName());
+  llvm::errs() << " ";
+
+  auto ret_type = f.getReturnType();
+  llvm::errs() << "ret type : ";
+  ret_type->print(llvm::errs());
+
+  llvm::errs() << "\n";
+
+  return false;
 }
 
 
