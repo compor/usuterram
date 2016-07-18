e83bddd3c6940354c429647e96ba442389bd87db
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-07-18 16:48:30 +0100

    allowuse in non-debug builds

--- llvm/looper/LooperPass.cpp
+++ llvm/looper/LooperPass.cpp
@@ -3,6 +3,9 @@
 //#include "llvm/IR/Type.h"
 #include "llvm/Analysis/LoopPass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Debug.h"
+
+#define DEBUG_TYPE "looper"
 
 
 namespace compor {
@@ -13,7 +16,7 @@ struct Looper : public llvm::LoopPass {
   Looper() : llvm::LoopPass(ID) {}
 
   bool runOnLoop(llvm::Loop *L, llvm::LPPassManager &LPM) override {
-    L->dump();
+    DEBUG(L->dump());
     llvm::errs() << "loop depth : " << L->getLoopDepth() << "\n";
     llvm::errs() << "loop blocks : " << L->getNumBlocks() << "\n";
     llvm::errs() << "loop id present : " << (L->getLoopID() ? "true" : "false") << "\n";
